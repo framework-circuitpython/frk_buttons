d77130f74017dc650738d13e1969480aa61d4a23
Nathan Woody <nathan@nathanwoody.com>
2022-12-28 05:35:42 +0000

    Commit to latest

--- src/frk_buttons.py
+++ src/frk_buttons.py
@@ -1,48 +1,42 @@
-from framework import Driver
 import keypad
+import asyncio
 
-class Buttons(Driver):
-    _defaults = {'sleep': 0.01,
-                 'value_when_pressed': True,
-                 'pull': True,
-                 'key_number': None,
-                 'pin_alias': None,
-                 'pin_name': None,
-                 'pressed': False,
-                 'released': True,
-                 'event': False,
-                 'on_pressed': [],
-                 'on_released': [],
-                 'on_event': []}
-
-    _get_set_del = {'sleep': 'g',
-                    'value_when_pressed': 'g',
-                    'pull': 'g',
-                    'key_number': 'g',
-                    'pin_alias': 'g',
-                    'pin_name': 'g',
-                    'pressed': 'gs',
-                    'released': 'gs',
-                    'event': 'gs',
-                    'on_pressed': 's',
-                    'on_released': 's',
-                    'on_event': 's'}
-
+class Buttons:
+    sleep = 0.01
+    value_when_pressed = True
+    pull = True
+    
+    key_number = None
+    pin_alias = None
+    pin_name = None
+    
+    event = False
+    pressed = False
+    released = False
+    on_event = []
+    on_pressed = []
+    on_released = []
+    
     def _init_device(self):
-        self._device = keypad.Keys(self._pins, value_when_pressed=self._value_when_pressed, pull=self._pull)
-
-    def _loop(self):
-        _event = self._device.events.get()
-        if _event:
-            if _event.pressed:
-                self._pressed = True
-                self._released = False
-                self._key_number = _event.key_number
-                self._pin_alias = self._raw_aliased_pins[_event.key_number][0]
-                self._pin_name = self._raw_aliased_pins[_event.key_number][1]
-                self._handle_event('pressed', self._pin_alias)
-            else:
-                self._pressed = False
-                self._released = False
-                self._handle_event('released')
-            self._handle_event('event')
+        self._device = keypad.Keys(self._pins, value_when_pressed=self.value_when_pressed, pull=self.pull)
+    
+    async def _run(self):
+        while True:
+            event = self._device.events.get()
+            if event:
+                if event.pressed:
+                    self._pressed = True
+                    self._released = False
+                    self._key_number = event.key_number
+                    self._pin_alias = self._raw_aliased_pins[event.key_number][0]
+                    self._pin_name = self._raw_aliased_pins[event.key_number][1]
+                    self._handle_event("pressed", self._pin_alias)
+                else:
+                    self._pressed = False
+                    self._released = True
+                    self._key_number = event.key_number
+                    self._pin_alias = self._raw_aliased_pins[event.key_number][0]
+                    self._pin_name = self._raw_aliased_pins[event.key_number][1]
+                    self._handle_event("released", self._pin_alias)
+                self._handle_event("event", self._pin_alias)
+            await asyncio.sleep(self._sleep)
